a85c606b50f30c09a50c2a049fba7454c1a0dd6a
Christopher Johnson <jinx6568@sover.net>
2024-04-23 14:21:40 -0400

    tests/PluginBasics.cpp should not still say Pamplejuce Demo

--- tests/PluginBasics.cpp
+++ tests/PluginBasics.cpp
@@ -20,7 +20,7 @@ TEST_CASE ("Plugin instance", "[instance]")
     SECTION ("name")
     {
         CHECK_THAT (testPlugin.getName().toStdString(),
-            Catch::Matchers::Equals ("Pamplejuce Demo"));
+            Catch::Matchers::Equals ("Meter"));
     }
 }
 
